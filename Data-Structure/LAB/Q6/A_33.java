c188a2b791c546750ae3584c905118361acbb95e
Dhairya2716 <dhairyadudkiya27@gmail.com>
2024-06-26 13:33:35 +0530

    LAB

---
+++ Data-Structure/LAB/Q6/A_33.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class A_33{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter 1st number : ");
+        int a = sc.nextInt();
+        System.out.println("Enter 2nd number : ");
+        int b = sc.nextInt();
+        System.out.println("Orignal number is "+a+" , "+b);
+        swap(a,b);
+    }
+    public static void swap(int a,int b){
+        int temp;
+        temp = a;
+        a = b;
+        b = temp;
+        System.out.println("Swapped number is : "+a+" , "+b);
+    }
+}
