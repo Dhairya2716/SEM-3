e810c7260aa91d973a467619f521fbaae89d029d
Dhairya2716 <dhairyadudkiya27@gmail.com>
2024-07-03 12:16:51 +0530

    LAB

--- Data-Structure/LAB/Q3/C_20.java
+++ Data-Structure/LAB/Q3/C_20.java
@@ -1,24 +1,31 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class C_20 {
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the number of names: ");
-        int n = sc.nextInt();
-        sc.nextLine(); 
-        
-        String[] names = new String[n];
-        System.out.println("Enter the names:");
-        for (int i = 0; i < n; i++) {
-            names[i] = sc.nextLine();
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the size of the array : ");
+    int n = sc.nextInt();        
+    String[] arr = new String[n];
+    System.out.println("Enter the Names : ");
+    for(int i = 0;i<n;i++){
+        arr[i] = sc.next();
+    }
+
+    for(int i = 0;i<n;i++){
+        for(int j = 0;j<n-1;j++){
+            if(arr[j].compareTo(arr[j+1])>0){
+                String temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
         }
-        
-        Arrays.sort(names);
-        
-        System.out.println("The names in alphabetical order are:");
-        for (String name : names) {
-            System.out.println(name);
-        }
-        sc.close();
+    }
+    System.out.println(" ");
+    System.out.println("Names in asecding order ---> ");
+    for(int i = 0;i<n;i++){
+        System.out.println(arr[i]);
+    }
+    sc.close();
     }
 }
